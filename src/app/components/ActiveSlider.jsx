6ca528682b26bd33bf4d002502e6f6021d0f4c38
@Purnesh1226 <purnesh.kalavagunta@clouddatanetworks.com>
2024-02-08 12:03:33 +0530

    Twelve Update

--- src/app/components/ActiveSlider.jsx
+++ src/app/components/ActiveSlider.jsx
@@ -6,9 +6,7 @@ import "swiper/css/free-mode";
 
 import { FreeMode, Pagination, Autoplay } from "swiper/modules";
 
-import { RxArrowTopRight } from "react-icons/rx";
 import Image from "next/image";
-// import { ServiceData } from "@/app/data/index";
 
 import {
   RxCrop,
@@ -18,12 +16,6 @@ import {
   RxRocket,
   RxAccessibility,
 } from "react-icons/rx";
-
-// import SpaceCity5 from "../assets/SpaceCity5.jpeg";
-// import SpaceCity6 from "../assets/SpaceCity6.jpeg";
-// import SpaceCity7 from "../assets/SpaceCity7.jpeg";
-// import SpaceCity8 from "../assets/SpaceCity8.jpeg";
-// import SpaceCity9 from "../assets/SpaceCity9.jpeg";
 
 const ActiveSlider = () => {
   const ServiceData = [
@@ -108,7 +100,7 @@ const ActiveSlider = () => {
 
   return (
     <div className="bg-gray-50 ">
-      <div className="relative pt-8 pb-4">
+      <div className="relative pt-4 pb-4">
         <div className="w-full">
           <div className="w-full px-4 mx-auto max-w-[1400px]">
             <div className="flex items-center justify-center flex-col bg-gray-50 ">
@@ -137,39 +129,6 @@ const ActiveSlider = () => {
               >
                 {ServiceData.map((item) => (
                   <SwiperSlide key={item.title}>
-                    {/* <div className="flex flex-col gap-6 mb-20 group relative shadow-lg text-white rounded-xl px-6 py-8 h-[250px] w-[215px] lg:h-[400px] lg:w-[350px] overflow-hidden cursor-pointer border-2 border-pink-500"> */}
-                    {/* <div
-                className="absolute inset-0 bg-cover bg-center border-2 border-red-700"
-                style={{ backgroundImage: `url(${item.backgroundImage})` }}
-              />
-
-              <div className="absolute inset-0 bg-black opacity-10 group-hover:opacity-50 " />
-              <div className="relative flex flex-col gap-3 border-2 border-red-600">
-                <item.icon className="text-blue-600 group-hover:text-blue-400 w-[32px] h-[32px]" />
-                <h1 className="text-xl lg:text-2xl">{item.title} </h1>
-                <p className="lg:text-[18px]">{item.content} </p>
-              </div> */}
-                    {/* <RxArrowTopRight className="absolute bottom-5 left-5 w-[35px] h-[35px] text-white group-hover:text-blue-500 group-hover:rotate-45 duration-100" /> */}
-                    {/* <div className="h-[250px] w-full lg:h-[400px] lg:w-full pb-20 border-2 border-blue-500">
-                <div className="flex flex-col justify-center items-center">
-                  <div className="border-2 border-green-500">
-                    <Image
-                      src={item.backgroundImage}
-                      alt=""
-                      width={176}
-                      height={176}
-                      className="rounded-full bg-white"
-                    ></Image>
-                  </div>
-                  <div>
-                    <h1>{item.title}</h1>
-                  </div>
-                  <div className="overflow-hidden">
-                    <p>{item.content}</p>
-                  </div>
-                </div>
-              </div> */}
-
                     <div className="rounded-xl bg-white shadow-[0_3px_10px_rgb(0,0,0,0.2)] h-88">
                       <div className="flex flex-col justify-center items-center mb-20 mt-8">
                         <div className="my-4">
@@ -182,7 +141,7 @@ const ActiveSlider = () => {
                           ></Image>
                         </div>
                         <div className="my-2">
-                          <h1 className="font-bold md:text-[20px] text-[22px] tracking-wide text-primaryColor">
+                          <h1 className="font-bold md:text-[20px] text-[22px] tracking-wide uppercase text-primaryColor">
                             {item.title}
                           </h1>
                         </div>
@@ -235,15 +194,13 @@ const ActiveSlider = () => {
                           </svg>
                         </div>
 
-                        <div className="mt-2 mb-4 ">
-                          <p className="text-[15px] tracking-wide mx-2">
+                        <div className="mt-2 mb-5">
+                          <p className="text-[15px] tracking-wide mx-2 font-medium text-gray-600 dark:text-gray-900">
                             {item.content}
                           </p>
                         </div>
                       </div>
                     </div>
-
-                    {/* </div> */}
                   </SwiperSlide>
                 ))}
               </Swiper>
