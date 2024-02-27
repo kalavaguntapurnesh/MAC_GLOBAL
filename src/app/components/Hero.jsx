6d1a3bc507edfc3b6f12d57d2f6f9fde9c2d9365
@Purnesh1226 <purnesh.kalavagunta@clouddatanetworks.com>
2024-02-27 21:08:38 +0530

    Thirty Nine

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -4,30 +4,45 @@ import { AiOutlineSearch } from "react-icons/ai";
 
 const Hero = () => {
   return (
-    <div className="w-full bg-white pt-28 pb-8 border-2 border-green-400 h-auto">
+    <div className="w-full bg-white pt-28 pb-8 h-auto">
       <div className="max-w-[1400px] m-auto grid md:grid-cols-2">
-        <div className="flex flex-col gap-4 ">
-          <div className="flex items-center lg:justify-start justify-center">
+        <div className="flex flex-col md:gap-3 gap-4">
+          {/* <div className="flex items-center lg:justify-start justify-center">
             <p className="uppercase text-[22px] text-[#20B486] font-medium py-2 lg:px-4 xl:px-2 lg:text-start text-center">
               start to success
             </p>
-          </div>
-          <div className=" flex items-center lg:justify-start justify-center">
-            <h1 className="xl:text-6xl lg:text-[48px] text-[44px] font-semibold py-2 lg:px-4 xl:px-2 xl:leading-[72px] lg:leading-[60px] lg:text-start text-center">
+          </div> */}
+          <div className=" flex items-center lg:justify-start justify-center mt-4">
+            <h1 className="xl:text-6xl lg:text-[48px] text-[48px] font-semibold lg:px-4 xl:px-2 xl:leading-[72px] lg:leading-[60px] lg:text-start text-center">
               Access to 5000+ Courses from 300
-              <span className="text-[#20B486]">
-                {" "}
-                Instructors & Institutions
-              </span>
+              <span className="text-colorOne"> Instructors & Institutions</span>
             </h1>
           </div>
+
           <div className=" flex items-center lg:justify-start justify-center">
-            <p className="lg:text-lg text-[16px] text-gray-900 py-2 lg:px-4 xl:px-2 lg:text-start text-center">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi
-              labore repellat.
+            <h1 className="lg:text-[24px] text-[24px] font-semibold lg:px-4 xl:px-2 xl:leading-[72px] lg:leading-[60px] lg:text-start text-center">
+              Top 1% Pre-vetted Coders | Dedicated Software Programmers.
+            </h1>
+          </div>
+
+          <div className=" flex items-center lg:justify-start justify-center">
+            <p className="lg:text-lg text-[16px] text-gray-900 lg:px-4 xl:px-2 lg:text-start text-center">
+              Hire best software engineers in India from AppsDevPro. An
+              award-winning software outsourcing company, we help businesses
+              meet the tech-talent.
             </p>
           </div>
-          <div className="lg:px-4 xl:px-2">
+
+          <div className=" flex items-center lg:justify-start justify-center">
+            <a
+              href="/contact"
+              className="bg-colorOne font-semibold outline-none
+              rounded-md hover:shadow-form transition duration-1000 ease-in-out hover:bg-colorTwo text-white px-8 py-2.5 text-center"
+            >
+              Get Started Now
+            </a>
+          </div>
+          {/* <div className="lg:px-4 xl:px-2">
             <form
               action=""
               className="bg-white border max-w-[500px] p-4 input-box-shadow rounded-md flex justify-between"
@@ -45,17 +60,17 @@ const Hero = () => {
                 />
               </button>
             </form>
-          </div>
+          </div> */}
         </div>
 
-        <div className="flex justify-center items-center ">
-          <Image
+        <div className="flex justify-center items-center">
+          {/* <Image
             src="/HERO.jpg"
             alt=""
             width={300}
-            height={300}
-            className="md:order-last h-11/12 w-5/6 border-2 border-red-400"
-          ></Image>
+            height={200}
+            className="md:order-last h-6/12 w-5/6 border-2 border-red-400"
+          ></Image> */}
         </div>
       </div>
     </div>
--- src/app/components/TrackRecord.jsx
+++ src/app/components/TrackRecord.jsx
@@ -11,38 +11,16 @@ const TrackRecord = () => {
       onEnter={() => setCounterOn(true)}
       onExit={() => setCounterOn(false)}
     >
-      <div className="bg-gray-50 py-8">
-        <div className="relative pt-4 pb-8">
+      <div className="bg-white pt-4 pb-8">
+        <div className="relative pb-8">
           <div className="w-full">
             <div className="w-full px-4 mx-auto max-w-[1400px]">
               <div className="justify-center w-full text-center pb-4">
                 <div className="w-full max-w-[14000px] mx-auto space-y-4 ">
-                  {/* <div className="flex flex-wrap flex-col items-center">
-                    <div className="flex items-center justify-center">
-                      <Image
-                        src="/left.svg"
-                        alt=""
-                        width={27}
-                        height={52}
-                      ></Image>
-                      <h1 className="text-4xl font-bold tracking-tight text-primaryColor dark:text-black mx-1">
-                        {" "}
-                        Our Tremendous Track Record
-                      </h1>
-                      <Image
-                        src="/right.svg"
-                        alt=""
-                        width={27}
-                        height={52}
-                      ></Image>
-                    </div>
-                    <div className="md:w-52 w-44 h-1 border-b-4 border-red-400 mt-2"></div>
-                  </div> */}
-
                   <div className="grid md:grid-cols-4 grid-cols-2 md:gap-8 gap-8 mx-auto justify-center items-center content-center">
                     <div className=" flex flex-col gap-1">
                       <div className=" h-12 flex justify-center items-center">
-                        <h1 className="font-bold text-4xl text-colorOne tracking-wider">
+                        <h1 className="font-bold text-5xl text-colorOne tracking-wider">
                           {counterOn && (
                             <CountUp
                               start={0}
@@ -62,7 +40,7 @@ const TrackRecord = () => {
                     </div>
                     <div className=" flex flex-col gap-1">
                       <div className=" h-12 flex justify-center items-center tracking-wider">
-                        <h1 className="font-bold text-4xl text-colorOne">
+                        <h1 className="font-bold text-5xl text-colorOne">
                           {counterOn && (
                             <CountUp
                               start={0}
@@ -82,7 +60,7 @@ const TrackRecord = () => {
                     </div>
                     <div className=" flex flex-col gap-1">
                       <div className=" h-12 flex justify-center items-center">
-                        <h1 className="font-bold text-4xl text-colorOne tracking-wider">
+                        <h1 className="font-bold text-5xl text-colorOne tracking-wider">
                           {counterOn && (
                             <CountUp
                               start={0}
@@ -167,7 +145,7 @@ const TrackRecord = () => {
                     </div>
                     <div className=" flex flex-col gap-1">
                       <div className=" h-12 flex justify-center items-center">
-                        <h1 className="font-bold text-4xl text-colorOne tracking-wider">
+                        <h1 className="font-bold text-5xl text-colorOne tracking-wider">
                           {counterOn && (
                             <CountUp
                               start={0}
@@ -185,16 +163,6 @@ const TrackRecord = () => {
                         </h1>
                       </div>
                     </div>
-                  </div>
-
-                  <div className="text-gray-600 dark:text-gray-800 pt-4 pb-4">
-                    <p className="font-medium leading-relaxed md:text-[17px] text-[16px] text-center tracking-wide">
-                      We offer overseas education services to top countries in
-                      the rundown of international education. Are you looking
-                      for abroad education services, MAC Global Services will
-                      encapsulate all from counselling to visa stamping. We
-                      offer services to study in countries like,
-                    </p>
                   </div>
                 </div>
               </div>
