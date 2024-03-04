c9a2121d35760d95d772cf8c28c456fb28273721
@Purnesh1226 <purnesh.kalavagunta@clouddatanetworks.com>
2024-03-04 10:06:18 +0530

    Sevnety

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -6,37 +6,33 @@ const Hero = () => {
   return (
     <div className="w-full bg-white pt-28 pb-8 h-auto ">
       <div className="max-w-[1400px] m-auto grid md:grid-cols-2 ">
-        <div className="flex flex-col md:gap-3 gap-4 ">
-          {/* <div className="flex items-center lg:justify-start justify-center">
-            <p className="uppercase text-[22px] text-[#20B486] font-medium py-2 lg:px-4 xl:px-2 lg:text-start text-center">
-              start to success
-            </p>
-          </div> */}
-          <div className=" flex items-center lg:justify-start justify-center mt-4 ">
-            <h1 className="xl:text-6xl lg:text-[48px] text-[48px] font-semibold lg:px-4 xl:px-2 xl:leading-[72px] lg:leading-[60px] lg:text-start text-center text-colorTwo">
-              Access to 5000+ Courses from 300
-              <span className="text-colorOne"> Instructors & Institutions</span>
+        <div className="flex flex-col gap-4 w-full">
+          <div className=" flex items-center lg:justify-start justify-center mt-4 mx-3">
+            <h1 className="xl:text-6xl text-[48px] font-bold lg:px-2 xl:leading-[72px] lg:leading-[60px] lg:text-start text-center text-colorTwo">
+              Find your right catch, in ocean
+              <span className="text-colorOne"> of opportunities</span>
             </h1>
           </div>
 
-          <div className="flex items-center lg:justify-start justify-center">
-            <h1 className="lg:text-[24px] text-[24px] font-semibold lg:px-4 xl:px-2 xl:leading-[72px] lg:leading-[60px] lg:text-start text-center">
-              Top 1% Pre-vetted Coders | Dedicated Software Programmers.
+          <div className="flex items-center lg:justify-start justify-center mx-3">
+            <h1 className="lg:text-[24px] text-[24px] font-semibold lg:px-2 lg:text-start text-center">
+              Want to study abroad this 2024 ?
             </h1>
           </div>
 
-          <div className="flex items-center lg:justify-start justify-center">
-            <p className="lg:text-lg text-[16px] text-gray-900 lg:px-4 xl:px-2 lg:text-start text-center">
-              Hire best software engineers in India from AppsDevPro. An
-              award-winning software outsourcing company, we help businesses
-              meet the tech-talent.
+          <div className="flex items-center lg:justify-start justify-center mx-3">
+            <p className="lg:text-lg text-[16px] text-gray-900 lg:px-2 lg:text-start text-center">
+              But you might feel a bit lost on how to get started. So, Mac does
+              the hard work for you! Everything you need to know is on one
+              platform. You will receive guidance throughout the whole
+              application process-all completely free.
             </p>
           </div>
 
-          <div className="flex items-center lg:justify-start justify-center md:mx-2">
+          <div className="flex items-center lg:justify-start justify-center md:mx-5">
             <a
               href="/contact"
-              className="bg-colorOne font-semibold outline-none
+              className="bg-colorTwo font-semibold outline-none
               rounded-md hover:shadow-form transition duration-1000 ease-in-out hover:bg-colorTwo text-white px-8 py-2.5 text-center"
             >
               Get Started Now
