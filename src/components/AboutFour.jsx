987cea5776fe4853b5150eacd2e949b50f8bede3
@Purnesh1226 <purnesh.kalavagunta@clouddatanetworks.com>
2024-07-03 09:12:16 +0530

    News letter

--- src/components/AboutFour.jsx
+++ src/components/AboutFour.jsx
@@ -10,22 +10,22 @@ const AboutFour = () => {
             <div className="justify-center w-full ">
               <div className="w-full max-w-[14000px] mx-auto space-y-4 ">
                 <section>
-                  <div class="container p-6 mx-auto space-y-12 lg:px-8">
+                  <div className="container p-6 mx-auto space-y-12 lg:px-8">
                     <div>
-                      <h2 class="text-3xl font-bold text-colorTwo text-center sm:text-5xl">
+                      <h2 className="text-3xl font-bold text-colorTwo text-center sm:text-5xl">
                         Why we are the best?
                       </h2>
-                      <p class="max-w-3xl text-colorTwo mx-auto mt-4 text-xl text-center ">
+                      <p className="max-w-3xl text-colorTwo mx-auto mt-4 text-xl text-center ">
                         Explore the latest features that enhance your learning
                         experience and make it even more exciting.
                       </p>
                     </div>
-                    <div class="grid lg:gap-8 lg:grid-cols-2 lg:items-center">
+                    <div className="grid lg:gap-8 lg:grid-cols-2 lg:items-center">
                       <div>
-                        <div class="mt-4 space-y-12">
-                          <div class="flex">
-                            <div class="flex-shrink-0">
-                              <div class="flex items-center justify-center w-12 h-12 rounded-md">
+                        <div className="mt-4 space-y-12">
+                          <div className="flex">
+                            <div className="flex-shrink-0">
+                              <div className="flex items-center justify-center w-12 h-12 rounded-md">
                                 <svg
                                   xmlns="http://www.w3.org/2000/svg"
                                   width="24"
@@ -33,9 +33,9 @@ const AboutFour = () => {
                                   viewBox="0 0 24 24"
                                   fill="none"
                                   stroke="currentColor"
-                                  stroke-width="2"
-                                  stroke-linecap="round"
-                                  stroke-linejoin="round"
+                                  strokeWidth="2"
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
                                   class="lucide lucide-rocket"
                                 >
                                   <path d="M4.5 16.5c-1.5 1.26-2 5-2 5s3.74-.5 5-2c.71-.84.7-2.13-.09-2.91a2.18 2.18 0 0 0-2.91-.09z"></path>
@@ -45,20 +45,20 @@ const AboutFour = () => {
                                 </svg>
                               </div>
                             </div>
-                            <div class="ml-4">
-                              <h4 class="text-lg font-medium text-colorOne">
+                            <div className="ml-4">
+                              <h4 className="text-lg font-medium text-colorOne">
                                 Application & Admission Assistance
                               </h4>
-                              <p class="mt-2 text-colorTwo">
+                              <p className="mt-2 text-colorTwo">
                                 Guiding students through the application
                                 process, helping with documentation, form
                                 filling, and ensuring all requirements are met.
                               </p>
                             </div>
                           </div>
-                          <div class="flex">
-                            <div class="flex-shrink-0">
-                              <div class="flex items-center justify-center w-12 h-12 rounded-md">
+                          <div className="flex">
+                            <div className="flex-shrink-0">
+                              <div className="flex items-center justify-center w-12 h-12 rounded-md">
                                 <svg
                                   xmlns="http://www.w3.org/2000/svg"
                                   width="24"
@@ -66,9 +66,9 @@ const AboutFour = () => {
                                   viewBox="0 0 24 24"
                                   fill="none"
                                   stroke="currentColor"
-                                  stroke-width="2"
-                                  stroke-linecap="round"
-                                  stroke-linejoin="round"
+                                  strokeWidth="2"
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
                                   class="lucide lucide-bookmark-plus"
                                 >
                                   <path d="m19 21-7-4-7 4V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v16z"></path>
@@ -77,11 +77,11 @@ const AboutFour = () => {
                                 </svg>
                               </div>
                             </div>
-                            <div class="ml-4">
-                              <h4 class="text-lg font-medium text-colorOne ">
+                            <div className="ml-4">
+                              <h4 className="text-lg font-medium text-colorOne ">
                                 Standardized Test Preparation
                               </h4>
-                              <p class="mt-2 text-colorTwo">
+                              <p className="mt-2 text-colorTwo">
                                 We offer coaching or resources to prepare
                                 students for standardized tests required for
                                 admission, such as TOEFL, IELTS, SAT, GRE, GMAT,
@@ -89,9 +89,9 @@ const AboutFour = () => {
                               </p>
                             </div>
                           </div>
-                          <div class="flex">
-                            <div class="flex-shrink-0">
-                              <div class="flex items-center justify-center w-12 h-12 rounded-md">
+                          <div className="flex">
+                            <div className="flex-shrink-0">
+                              <div className="flex items-center justify-center w-12 h-12 rounded-md">
                                 <svg
                                   xmlns="http://www.w3.org/2000/svg"
                                   width="24"
@@ -99,9 +99,9 @@ const AboutFour = () => {
                                   viewBox="0 0 24 24"
                                   fill="none"
                                   stroke="currentColor"
-                                  stroke-width="2"
-                                  stroke-linecap="round"
-                                  stroke-linejoin="round"
+                                  strokeWidth="2"
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
                                   class="lucide lucide-video"
                                 >
                                   <path d="m22 8-6 4 6 4V8Z"></path>
@@ -116,20 +116,20 @@ const AboutFour = () => {
                                 </svg>
                               </div>
                             </div>
-                            <div class="ml-4">
-                              <h4 class="text-lg font-medium text-colorOne ">
+                            <div className="ml-4">
+                              <h4 className="text-lg font-medium text-colorOne ">
                                 Alumni Students Networking
                               </h4>
-                              <p class="mt-2 text-colorTwo">
+                              <p className="mt-2 text-colorTwo">
                                 Connecting students with alumni who have studied
                                 abroad, providing valuable insights and
                                 networking opportunities.
                               </p>
                             </div>
                           </div>
-                          <div class="flex">
-                            <div class="flex-shrink-0">
-                              <div class="flex items-center justify-center w-12 h-12 rounded-md">
+                          <div className="flex">
+                            <div className="flex-shrink-0">
+                              <div className="flex items-center justify-center w-12 h-12 rounded-md">
                                 <svg
                                   xmlns="http://www.w3.org/2000/svg"
                                   width="24"
@@ -137,9 +137,9 @@ const AboutFour = () => {
                                   viewBox="0 0 24 24"
                                   fill="none"
                                   stroke="currentColor"
-                                  stroke-width="2"
-                                  stroke-linecap="round"
-                                  stroke-linejoin="round"
+                                  strokeWidth="2"
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
                                   class="lucide lucide-file-question"
                                 >
                                   <path d="M14.5 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7.5L14.5 2z"></path>
@@ -148,11 +148,11 @@ const AboutFour = () => {
                                 </svg>
                               </div>
                             </div>
-                            <div class="ml-4">
-                              <h4 class="text-lg font-medium text-colorOne ">
+                            <div className="ml-4">
+                              <h4 className="text-lg font-medium text-colorOne ">
                                 Language & Cultural Training
                               </h4>
-                              <p class="mt-2 text-colorTwo">
+                              <p className="mt-2 text-colorTwo">
                                 We provide language training or recommend
                                 language courses to help students improve their
                                 proficiency in the language.
@@ -161,17 +161,114 @@ const AboutFour = () => {
                           </div>
                         </div>
                       </div>
-                      <div aria-hidden="true" class="mt-10 lg:mt-0">
+                      <div aria-hidden="true" className="mt-10 lg:mt-0">
                         <img
                           width="600"
                           height="600"
                           src="https://images.unsplash.com/photo-1603573355706-3f15d98cf100?q=80&w=3858&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                          class="mx-auto rounded-lg shadow-lg dark-bg-gray-500"
+                          className="mx-auto rounded-lg shadow-lg dark-bg-gray-500"
                         />
                       </div>
                     </div>
                   </div>
                 </section>
+
+                <div className="relative isolate overflow-hidden py-8 md:py-24">
+                  <div className="mx-auto px-6 lg:px-8">
+                    <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-2">
+                      <div className="max-w-xl lg:max-w-lg">
+                        <h2 className="text-3xl font-bold tracking-tight text-colorTwo sm:text-4xl">
+                          Subscribe to our newsletter.
+                        </h2>
+                        <p className="mt-4 text-lg leading-8 text-colorTwo">
+                          Nostrud amet eu ullamco nisi aute in ad minim nostrud
+                          adipisicing velit quis. Duis tempor incididunt dolore.
+                        </p>
+                        <div className="mt-6 flex max-w-md gap-x-4">
+                          <label for="email-address" class="sr-only">
+                            Email address
+                          </label>
+                          <input
+                            id="email-address"
+                            name="email"
+                            type="email"
+                            autocomplete="email"
+                            required
+                            className="min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-gray-500 shadow-sm ring-1 ring-inset sm:text-sm sm:leading-6"
+                            placeholder="Enter your email"
+                          />
+                          <button
+                            type="submit"
+                            className="flex-none rounded-md bg-colorOne px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-colorTwo focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+                          >
+                            Subscribe
+                          </button>
+                        </div>
+                      </div>
+                      <dl className="grid grid-cols-1 gap-x-8 gap-y-10 sm:grid-cols-2 lg:pt-2">
+                        <div className="flex flex-col items-start">
+                          <div className="rounded-md bg-white/5 p-2 ring-1 ring-white/10">
+                            <svg
+                              class="h-6 w-6 text-colorTwo"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              stroke-width="1.5"
+                              stroke="currentColor"
+                              aria-hidden="true"
+                            >
+                              <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z"
+                              />
+                            </svg>
+                          </div>
+                          <dt className="mt-4 font-semibold text-colorTwo">
+                            Weekly articles
+                          </dt>
+                          <dd className="mt-2 leading-7 text-colorTwo">
+                            Non laboris consequat cupidatat laborum magna.
+                            Eiusmod non irure cupidatat duis commodo amet.
+                          </dd>
+                        </div>
+                        <div className="flex flex-col items-start">
+                          <div className="rounded-md bg-white/5 p-2 ring-1 ring-white/10">
+                            <svg
+                              class="h-6 w-6 text-colorTwo"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              stroke-width="1.5"
+                              stroke="currentColor"
+                              aria-hidden="true"
+                            >
+                              <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                d="M10.05 4.575a1.575 1.575 0 10-3.15 0v3m3.15-3v-1.5a1.575 1.575 0 013.15 0v1.5m-3.15 0l.075 5.925m3.075.75V4.575m0 0a1.575 1.575 0 013.15 0V15M6.9 7.575a1.575 1.575 0 10-3.15 0v8.175a6.75 6.75 0 006.75 6.75h2.018a5.25 5.25 0 003.712-1.538l1.732-1.732a5.25 5.25 0 001.538-3.712l.003-2.024a.668.668 0 01.198-.471 1.575 1.575 0 10-2.228-2.228 3.818 3.818 0 00-1.12 2.687M6.9 7.575V12m6.27 4.318A4.49 4.49 0 0116.35 15m.002 0h-.002"
+                              />
+                            </svg>
+                          </div>
+                          <dt className="mt-4 font-semibold text-colorTwo">
+                            No spam
+                          </dt>
+                          <dd className="mt-2 leading-7 text-colorTwo">
+                            Officia excepteur ullamco ut sint duis proident non
+                            adipisicing. Voluptate incididunt anim.
+                          </dd>
+                        </div>
+                      </dl>
+                    </div>
+                  </div>
+                  <div
+                    className="absolute left-1/2 top-0 -z-10 -translate-x-1/2 blur-3xl xl:-top-6"
+                    aria-hidden="true"
+                  >
+                    {/* <div
+                      class="aspect-[1155/678] w-[72.1875rem] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30"
+                      style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"
+                    ></div> */}
+                  </div>
+                </div>
               </div>
             </div>
           </div>
