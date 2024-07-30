d2d940c4b5aaa7ed40d45455afe011c654a985ec
@Purnesh1226 <purnesh.kalavagunta@clouddatanetworks.com>
2024-07-30 22:59:10 +0530

    ther

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -4,6 +4,12 @@ import { IoMdArrowRoundUp } from "react-icons/io";
 import { classNames } from "../components/classNames";
 
 const ScrollToTop = () => {
+  const phoneNumber = "9347106983"; // Replace with your phone number
+  const message = "This is for the testing purposes of whatsapp updates..."; // Pre-filled message
+  const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(
+    message
+  )}`;
+
   const [isVisible, setIsVisible] = useState(false);
   const toggleVisibility = () => {
     if (window.pageYOffset > 200) {
@@ -30,16 +36,17 @@ const ScrollToTop = () => {
 
   return (
     <div className="fixed bottom-6 md:right-4 right-2 z-10 rounded-full">
-      <button
-        type="button"
-        onClick={scrollToTop}
+      <a
+        // type="button"
+        // onClick={scrollToTop}
+        href={whatsappUrl}
         className={classNames(
           isVisible ? "opacity-100" : "opacity-0",
           "bg-colorTwo hover:bg-colorOne transition duration-1000 ease-in-out inline-flex items-center rounded-full md:p-3 p-2 text-white shadow-sm "
         )}
       >
         <IoMdArrowRoundUp className="h-6 w-6" aria-hidden="true" />
-      </button>
+      </a>
     </div>
   );
 };
